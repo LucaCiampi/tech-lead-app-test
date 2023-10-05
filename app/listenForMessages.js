2e3e1d3394031cfe86d4fa81da89d3dc58189d0b
LucaCiampi <luca.ciampi@edu.gobelins.fr>
2023-10-05 14:18:10 +0200

    :sparkles: Added zip images download

---
+++ app/listenForMessages.js
@@ -0,0 +1,69 @@
+// Copyright 2019-2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// This is a generated sample, using the typeless sample bot. Please
+// look for the source TypeScript sample (.ts) for modifications.
+'use strict';
+
+/**
+ * This application demonstrates how to perform basic operations on
+ * subscriptions with the Google Cloud Pub/Sub API.
+ *
+ * For more information, see the README.md under /pubsub and the documentation
+ * at https://cloud.google.com/pubsub/docs.
+ */
+
+// sample-metadata:
+//   title: Listen For Messages
+//   description: Listens for messages from a subscription.
+//   usage: node listenForMessages.js <subscription-name-or-id> [timeout-in-seconds]
+
+// [START pubsub_subscriber_async_pull]
+// [START pubsub_quickstart_subscriber]
+
+// Imports the Google Cloud client library
+const {PubSub} = require('@google-cloud/pubsub');
+
+// Creates a client; cache this for further use
+const pubSubClient = new PubSub();
+
+function listenForMessages(subscriptionNameOrId, timeout) {
+  // References an existing subscription
+  const subscription = pubSubClient.subscription(subscriptionNameOrId);
+
+  // Create an event handler to handle messages
+  let messageCount = 0;
+  const messageHandler = message => {
+    console.log(`Received message ${message.id}:`);
+    console.log(`\tData: ${message.data}`);
+    console.log(`\tAttributes: ${message.attributes}`);
+    messageCount += 1;
+
+    // "Ack" (acknowledge receipt of) the message
+    message.ack();
+  };
+
+  // Listen for new messages until timeout is hit
+  subscription.on('message', messageHandler);
+
+  // Wait a while for the subscription to run. (Part of the sample only.)
+  setTimeout(() => {
+    subscription.removeListener('message', messageHandler);
+    console.log(`${messageCount} message(s) received.`);
+  }, timeout * 1000);
+}
+// [END pubsub_subscriber_async_pull]
+// [END pubsub_quickstart_subscriber]
+
+module.exports = listenForMessages;
--- app/route.js
+++ app/route.js
@@ -1,5 +1,7 @@
 const formValidator = require('./form_validator');
 const photoModel = require('./photo_model');
+const request = require('request');
+const ZipStream = require('zip-stream');
 
 function route(app) {
   app.get('/', (req, res) => {
@@ -35,6 +37,39 @@ function route(app) {
       })
       .catch(error => {
         return res.status(500).send({ error });
+      });
+  });
+
+  app.post('/zip', (req, res) => {
+    const tags = req.query.tags;
+
+    var zip = new ZipStream();
+    zip.pipe(res);
+
+    // Obtenez les photos à partir de votre modèle
+    photoModel
+      .getFlickrPhotos(tags)
+      .then(photos => {
+        // Transformez les résultats en URLs d'images et les ajoutez à la queue
+        const queue = photos.map(photo => {
+          return { name: `${photo.title}.jpg`, url: photo.media.b };
+        });
+
+        function addNextFile() {
+          var elem = queue.shift();
+          var stream = request(elem.url);
+          zip.entry(stream, { name: elem.name }, err => {
+            if (err) throw err;
+            if (queue.length > 0) addNextFile();
+            else zip.finalize();
+          });
+        }
+
+        addNextFile();
+      })
+      .catch(err => {
+        console.error('Error:', err);
+        res.status(500).send('Internal Server Error');
       });
   });
 }
